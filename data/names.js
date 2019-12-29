329765215a799c422abe1518be5ac808e8020ea7
Evan Morrison <evan.morrison@tazworks.com>
2019-12-29 00:24:21 -0700

    add data list and additional demos

---
+++ data/names.js
@@ -0,0 +1,625 @@
+const namesList = [
+	{
+		"name": "Jenna Alexander"
+	},
+	{
+		"name": "Quemby Bolton"
+	},
+	{
+		"name": "Aurora Burns"
+	},
+	{
+		"name": "Kerry Mejia"
+	},
+	{
+		"name": "Noelani Woods"
+	},
+	{
+		"name": "Juliet Sandoval"
+	},
+	{
+		"name": "Nadine Finley"
+	},
+	{
+		"name": "Jessica Brown"
+	},
+	{
+		"name": "Lucy Orr"
+	},
+	{
+		"name": "Nadine Richardson"
+	},
+	{
+		"name": "Keane Warner"
+	},
+	{
+		"name": "Rylee Obrien"
+	},
+	{
+		"name": "Irene Molina"
+	},
+	{
+		"name": "Chelsea Le"
+	},
+	{
+		"name": "Brynne Yang"
+	},
+	{
+		"name": "Veda Head"
+	},
+	{
+		"name": "Kermit Stevenson"
+	},
+	{
+		"name": "Hilel Puckett"
+	},
+	{
+		"name": "Jackson Head"
+	},
+	{
+		"name": "Samuel Collins"
+	},
+	{
+		"name": "Brenden Velazquez"
+	},
+	{
+		"name": "Vivien Baird"
+	},
+	{
+		"name": "Eric Thompson"
+	},
+	{
+		"name": "Murphy Delaney"
+	},
+	{
+		"name": "Odessa Spencer"
+	},
+	{
+		"name": "Keely Curry"
+	},
+	{
+		"name": "Nehru Macias"
+	},
+	{
+		"name": "Richard Ochoa"
+	},
+	{
+		"name": "Leo Cote"
+	},
+	{
+		"name": "Leroy Torres"
+	},
+	{
+		"name": "Blair Middleton"
+	},
+	{
+		"name": "Sophia Hendricks"
+	},
+	{
+		"name": "Kessie Gallegos"
+	},
+	{
+		"name": "Nadine Stark"
+	},
+	{
+		"name": "Jasper Gates"
+	},
+	{
+		"name": "Derek Gilmore"
+	},
+	{
+		"name": "Nicholas Tanner"
+	},
+	{
+		"name": "Lillith Osborne"
+	},
+	{
+		"name": "Idona Jackson"
+	},
+	{
+		"name": "McKenzie Valdez"
+	},
+	{
+		"name": "Yuri Myers"
+	},
+	{
+		"name": "Keiko Stokes"
+	},
+	{
+		"name": "Margaret Barron"
+	},
+	{
+		"name": "Danielle Tucker"
+	},
+	{
+		"name": "Nomlanga Garner"
+	},
+	{
+		"name": "Dominic Meyers"
+	},
+	{
+		"name": "Myles Wynn"
+	},
+	{
+		"name": "Alfonso Keller"
+	},
+	{
+		"name": "Kevin Franco"
+	},
+	{
+		"name": "Holly Summers"
+	},
+	{
+		"name": "Kyla Forbes"
+	},
+	{
+		"name": "Jael Clemons"
+	},
+	{
+		"name": "Patrick Merritt"
+	},
+	{
+		"name": "Leila Clarke"
+	},
+	{
+		"name": "Mohammad William"
+	},
+	{
+		"name": "Clio Steele"
+	},
+	{
+		"name": "Yolanda Lindsey"
+	},
+	{
+		"name": "Justin Logan"
+	},
+	{
+		"name": "Charissa Parker"
+	},
+	{
+		"name": "Harper Peters"
+	},
+	{
+		"name": "Ingrid Mcbride"
+	},
+	{
+		"name": "Hanna Gray"
+	},
+	{
+		"name": "Vernon Garrett"
+	},
+	{
+		"name": "Allen Richmond"
+	},
+	{
+		"name": "Mufutau Vinson"
+	},
+	{
+		"name": "Nissim Horn"
+	},
+	{
+		"name": "Dane Mullen"
+	},
+	{
+		"name": "Cynthia Bush"
+	},
+	{
+		"name": "Nerea Burch"
+	},
+	{
+		"name": "Gregory Lawrence"
+	},
+	{
+		"name": "Bernard Hatfield"
+	},
+	{
+		"name": "Kuame Fernandez"
+	},
+	{
+		"name": "Adria Fox"
+	},
+	{
+		"name": "Cassandra Owen"
+	},
+	{
+		"name": "Nicholas Walls"
+	},
+	{
+		"name": "Andrew Koch"
+	},
+	{
+		"name": "Arden Nguyen"
+	},
+	{
+		"name": "Quamar Stephenson"
+	},
+	{
+		"name": "Clark Patrick"
+	},
+	{
+		"name": "Hunter Carney"
+	},
+	{
+		"name": "Jarrod Douglas"
+	},
+	{
+		"name": "Eric Whitehead"
+	},
+	{
+		"name": "Imelda Hodge"
+	},
+	{
+		"name": "Melissa Hobbs"
+	},
+	{
+		"name": "Galvin Irwin"
+	},
+	{
+		"name": "Nadine Moses"
+	},
+	{
+		"name": "Nina Chang"
+	},
+	{
+		"name": "Kristen Becker"
+	},
+	{
+		"name": "Harding Rivers"
+	},
+	{
+		"name": "Todd Avery"
+	},
+	{
+		"name": "Daniel Navarro"
+	},
+	{
+		"name": "Evan Gay"
+	},
+	{
+		"name": "Karina Shannon"
+	},
+	{
+		"name": "Grant Ingram"
+	},
+	{
+		"name": "Kelsey Clarke"
+	},
+	{
+		"name": "Ginger Neal"
+	},
+	{
+		"name": "Nina Kelly"
+	},
+	{
+		"name": "Carson Lane"
+	},
+	{
+		"name": "Michelle Sutton"
+	},
+	{
+		"name": "Ross House"
+	},
+	{
+		"name": "Nigel Snow"
+	},
+	{
+		"name": "Ivan Blair"
+	},
+	{
+		"name": "Sawyer Villarreal"
+	},
+	{
+		"name": "Naida Lopez"
+	},
+	{
+		"name": "Ora Murphy"
+	},
+	{
+		"name": "Brynn Rowe"
+	},
+	{
+		"name": "Deborah Aguirre"
+	},
+	{
+		"name": "Keelie Hendrix"
+	},
+	{
+		"name": "Donovan Fowler"
+	},
+	{
+		"name": "Giselle Booker"
+	},
+	{
+		"name": "Pascale Good"
+	},
+	{
+		"name": "Akeem Alston"
+	},
+	{
+		"name": "Chelsea Webster"
+	},
+	{
+		"name": "Debra Peterson"
+	},
+	{
+		"name": "Brenden Conrad"
+	},
+	{
+		"name": "Rowan Weeks"
+	},
+	{
+		"name": "Emmanuel Robles"
+	},
+	{
+		"name": "Savannah Lee"
+	},
+	{
+		"name": "Eleanor Bond"
+	},
+	{
+		"name": "Nehru Russell"
+	},
+	{
+		"name": "Halee Avery"
+	},
+	{
+		"name": "Kevin Buckner"
+	},
+	{
+		"name": "Jorden Barlow"
+	},
+	{
+		"name": "Nissim Reid"
+	},
+	{
+		"name": "Rinah Bartlett"
+	},
+	{
+		"name": "Chase Ryan"
+	},
+	{
+		"name": "Remedios Mullins"
+	},
+	{
+		"name": "Upton Baxter"
+	},
+	{
+		"name": "Aretha Short"
+	},
+	{
+		"name": "Zelda Torres"
+	},
+	{
+		"name": "Gillian Stewart"
+	},
+	{
+		"name": "Florence Pennington"
+	},
+	{
+		"name": "Ezekiel Gillespie"
+	},
+	{
+		"name": "Illiana Gallegos"
+	},
+	{
+		"name": "Valentine Reynolds"
+	},
+	{
+		"name": "Coby Burton"
+	},
+	{
+		"name": "Charles Hines"
+	},
+	{
+		"name": "Wing Myers"
+	},
+	{
+		"name": "Tara Klein"
+	},
+	{
+		"name": "Raymond Chandler"
+	},
+	{
+		"name": "Timothy Leon"
+	},
+	{
+		"name": "Chase Garcia"
+	},
+	{
+		"name": "Illiana Booker"
+	},
+	{
+		"name": "Ulysses Best"
+	},
+	{
+		"name": "Karyn Peters"
+	},
+	{
+		"name": "Quinlan Bailey"
+	},
+	{
+		"name": "Germane Dyer"
+	},
+	{
+		"name": "Fiona Sloan"
+	},
+	{
+		"name": "Allistair Marsh"
+	},
+	{
+		"name": "Malcolm Frye"
+	},
+	{
+		"name": "Anne Gomez"
+	},
+	{
+		"name": "Odessa Wells"
+	},
+	{
+		"name": "Barclay Baird"
+	},
+	{
+		"name": "Zachary Nguyen"
+	},
+	{
+		"name": "Echo Sharp"
+	},
+	{
+		"name": "Alexandra Maddox"
+	},
+	{
+		"name": "Carly Joyce"
+	},
+	{
+		"name": "Beau Wilkinson"
+	},
+	{
+		"name": "Thomas Mathis"
+	},
+	{
+		"name": "Orlando Davis"
+	},
+	{
+		"name": "Whitney May"
+	},
+	{
+		"name": "Natalie Mooney"
+	},
+	{
+		"name": "Dennis Harper"
+	},
+	{
+		"name": "Ezekiel Jones"
+	},
+	{
+		"name": "Vernon Perry"
+	},
+	{
+		"name": "Ali Quinn"
+	},
+	{
+		"name": "Merrill Solis"
+	},
+	{
+		"name": "Joel Pacheco"
+	},
+	{
+		"name": "Chancellor Carney"
+	},
+	{
+		"name": "Yoshi Barrera"
+	},
+	{
+		"name": "Nerea Bright"
+	},
+	{
+		"name": "Laith Slater"
+	},
+	{
+		"name": "Orlando Hutchinson"
+	},
+	{
+		"name": "Alexandra Morin"
+	},
+	{
+		"name": "Noel Andrews"
+	},
+	{
+		"name": "Sybill Garrison"
+	},
+	{
+		"name": "Daniel Chaney"
+	},
+	{
+		"name": "Cain Nicholson"
+	},
+	{
+		"name": "Rhona Stone"
+	},
+	{
+		"name": "Orson Chaney"
+	},
+	{
+		"name": "Nero Hart"
+	},
+	{
+		"name": "Blair Austin"
+	},
+	{
+		"name": "Elvis Sargent"
+	},
+	{
+		"name": "Natalie Pruitt"
+	},
+	{
+		"name": "Evangeline Murray"
+	},
+	{
+		"name": "Nell Steele"
+	},
+	{
+		"name": "Carly Davidson"
+	},
+	{
+		"name": "Veronica Woods"
+	},
+	{
+		"name": "Cameran Bright"
+	},
+	{
+		"name": "Nathan Hardy"
+	},
+	{
+		"name": "Elaine Ayala"
+	},
+	{
+		"name": "Nichole Walton"
+	},
+	{
+		"name": "Melissa Merrill"
+	},
+	{
+		"name": "Brynn Owen"
+	},
+	{
+		"name": "Merritt Bell"
+	},
+	{
+		"name": "Chastity Grimes"
+	},
+	{
+		"name": "Mallory Forbes"
+	},
+	{
+		"name": "Coby Slater"
+	},
+	{
+		"name": "Uriel Mueller"
+	},
+	{
+		"name": "Wynter Barron"
+	}
+];
+
+export const namesGrouped = ((list) => {
+  return list.map((n, i) => {
+    let lastName = n.name.split(" ")[1];
+    let sortGroupLabel = lastName.charAt(0);
+    let sortGroupOrder = sortGroupLabel;
+    return({
+      label: n.name,
+      value: i + 1,
+      sortGroupLabel,
+      sortGroupOrder
+    })
+  })
+})(namesList)
+
+export const names = ((list) => {
+  return list.map((n, i) => {
+    return({
+      label: n.name,
+      value: i + 1
+    })
+  })
+})(namesList)
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,47 @@
 import React, { useRef } from 'react'
 import FormLinker from 'form-linker'
 import MDXContent from './index.mdx'
+import { nfl, nflGrouped } from '../data/nfl'
+import { names, namesGrouped} from '../data/names'
 import Field from '../components/molecules/field'
 
-const demo1List = [
-  {label: "Red", value: "red"},
-  {label: "Yellow", value: "yellow"},
-  {label: "Blue", value: "blue"},
-]
 const MultiSelectDemo = props => {
   const formLinker = useRef(new FormLinker({
     data: {
-      demo1: []
+      demo1: [],
+      demo2: [],
+      demo3: [],
+      demo4: [],
     },
     schema: {
-      demo1: "array"
+      demo1: "array",
+      demo2: "array",
+      demo3: "array",
+      demo4: "array",
     }
   }))
 
   return(
     <div css={{width: "100%", display: "flex", justifyContent: "center"}}>
       <div css={{flex: 1, maxWidth: 1200, margin: "20px 8px"}}>
-        <MDXContent/>
-        <div css={{maxWidth: 600, marginTop: 30}}>
-          <Field formLinker={formLinker.current} name="demo1" label="Multiselect" checkboxes type="multiSelect" options={demo1List}/>
+        <div css={{h1: {background: "#38f", color: "#fff", padding: 8}, code: {background: "rgba(150, 150, 150, 0.3)"}}}>
+          <MDXContent/>
+        </div>
+        <div css={{display: "flex", justifyContent: "space-around"}}>
+          <div css={{flex: 1, maxWidth: 400, marginTop: 30}}>
+            <Field formLinker={formLinker.current} name="demo1" label="Contacts" checkboxes showAllSelected type="multiSelect" options={namesGrouped}/>
+          </div>
+          <div css={{flex: 1, maxWidth: 500, marginTop: 30}}>
+            <Field formLinker={formLinker.current} name="demo2" label="Contacts" checkboxes columns={3} type="multiSelect" options={names}/>
+          </div>
+        </div>
+        <div css={{display: "flex", justifyContent: "space-around"}}>
+          <div css={{flex: 1, maxWidth: 300, marginTop: 30}}>
+            <Field formLinker={formLinker.current} name="demo3" label="Teams" checkboxes columns={1} showAllSelected type="multiSelect" options={nflGrouped}/>
+          </div>
+          <div css={{flex: 1, maxWidth: 800, marginTop: 30}}>
+            <Field formLinker={formLinker.current} name="demo4" label="Teams" checkboxes columns={4} type="multiSelect" options={nfl}/>
+          </div>
         </div>
       </div>
     </div>
